e24f800d413121e130498964152f960552e62cc5
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-10 00:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 //document.body.innerText="DOM load success";
-document.addEventListener("DomContentLoaded",()=>{
-	this.body.textContent="DOM load success";
+document.addEventListener("DOMContentLoaded",()=>{
+	document.body.innerText="DOM load success";
 });
